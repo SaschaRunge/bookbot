2574ca7dc2c9a1c1af40917a409b0f96238ae56b
SaschaRunge <sascharunge89@yahoo.de>
2025-11-24 09:52:42 +0100

    improved error handling

--- main.py
+++ main.py
@@ -9,11 +9,6 @@ def get_book_text(path_to_file):
         return f.read()
     return None
 
-def is_valid_call():
-    if len(sys.argv) != 2:
-        return False
-    return True
-
 def print_report(word_count, sorted_char_count, filepath_to_book):
     print(f"============ BOOKBOT ============")
     print(f"Analyzing book found at {filepath_to_book}...")
@@ -28,13 +23,14 @@ def print_report(word_count, sorted_char_count, filepath_to_book):
     print("============= END ===============")
 
 def main():
-    if not is_valid_call():
-        print("Usage: python3 main.py <path_to_book>")
-        return  
-    
-    filepath_to_book = sys.argv[1]
 
-    book_text = get_book_text(filepath_to_book)
+    try:    
+        filepath_to_book = sys.argv[1]
+        book_text = get_book_text(filepath_to_book)
+    except:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
     word_count = get_word_count(book_text)
     char_count = get_char_count(book_text)
     sorted_char_count = get_sorted_char_count(char_count)
