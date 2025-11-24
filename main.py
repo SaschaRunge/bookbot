defdd2cde788b25e999e8db8227b6566fa7b4e60
SaschaRunge <sascharunge89@yahoo.de>
2025-11-24 09:13:44 +0100

    completed report and filepath input

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
+import sys
+
 from stats import get_word_count
 from stats import get_char_count
+from stats import get_sorted_char_count
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
     return None
 
+def is_valid_call():
+    if len(sys.argv) != 2:
+        return False
+    return True
+
+def print_report(word_count, sorted_char_count, filepath_to_book):
+    print(f"============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath_to_book}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("----------- Character Count ----------")
+
+    for char in sorted_char_count:
+        if (char["char"].isalpha()):
+            print(f"{char["char"]}: {char["num"]}")
+
+    print("============= END ===============")
+
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    if not is_valid_call():
+        print("Usage: python3 main.py <path_to_book>")
+        return  
+    
+    filepath_to_book = sys.argv[1]
+
+    book_text = get_book_text(filepath_to_book)
     word_count = get_word_count(book_text)
     char_count = get_char_count(book_text)
-    print(f"Found {word_count} total words", char_count)
+    sorted_char_count = get_sorted_char_count(char_count)
+        
+    print_report(word_count, sorted_char_count, filepath_to_book)
 
 main()
--- stats.py
+++ stats.py
@@ -10,5 +10,21 @@ def get_char_count(text):
         char_count[char.lower()] += 1
     return char_count
 
+def sort_on(items):
+    return items["num"]
+
+def get_sorted_char_count(char_count):
+    sorted_char_count = []
+
+    for char in char_count:
+        sorted_char_count.append({"char": char, "num": char_count[char]})
+
+    sorted_char_count.sort(reverse=True, key=sort_on)
+
+    return sorted_char_count
+
+
+
+
 
     
